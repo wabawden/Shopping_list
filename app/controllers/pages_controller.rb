78449903a8665810b6748feaad3bc64c3ed3da12
Bill Bawden <wabawden@aol.com>
2020-10-28 15:58:33 +0000

    reroute home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if current_user
+      redirect_to items_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
