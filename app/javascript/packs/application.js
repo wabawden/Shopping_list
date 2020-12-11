0f7c295fadd9a0e4f364603c97d8115ca9cf3f77
Bill Bawden <wabawden@aol.com>
2020-12-11 11:00:02 +0000

    added sortablejs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { Sortable, MultiDrag, Swap, OnSpill, AutoScroll } from "sortablejs";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,4 +32,32 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  
+  
+
+
+var el = document.getElementById("items");
+Sortable.create(el, {
+  group: "localStorage-example",
+  store: {
+    /**
+     * Get the order of elements. Called once during initialization.
+     * @param   {Sortable}  sortable
+     * @returns {Array}
+     */
+    get: function (sortable) {
+      var order = localStorage.getItem(sortable.options.group.name);
+      return order ? order.split("|") : [];
+    },
+    
+    /**
+     * Save the order of elements. Called onEnd (when the item is dropped).
+     * @param {Sortable}  sortable
+     */
+    set: function (sortable) {
+      var order = sortable.toArray();
+      localStorage.setItem(sortable.options.group.name, order.join("|"));
+    },
+  },
+});
 });
